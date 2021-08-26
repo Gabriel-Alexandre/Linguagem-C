21f62078aca2b3827053437053a5b6d94eb6b41d
Gabriel Alexandre Carvalho <82059012+Gabriel-Alexandre@users.noreply.github.com>
2021-08-26 00:28:51 +0000

    feat(Listinha_01 ED): Resolvendo questão 9

--- Estrutura_Dados/Lista_01/Q_09/q_09.c
+++ Estrutura_Dados/Lista_01/Q_09/q_09.c
@@ -3,15 +3,43 @@
 
 int main() {
     int l, c;
+    int **mat;
+
+    // Alocação dinâmica de matriz
 
     printf("Numero de linhas: ");
     scanf("%d", &l);
     printf("Numero de colunas: ");
     scanf("%d", &c);
 
-    // int *m = (int *) malloc(l*sizeof(int));
+    mat = malloc(l * sizeof(int*));
 
-    // Não sei como fazer!
+    for(int i = 0; i < l; i++) {
+      mat[i] = (int *) malloc(c * sizeof(int));
+    }
 
+    // Preferir usar o for aninhado a usar o for com mais de uma variável de contagem.
+    // O Segundo tipo da problema em alguns casos, então é melhor evitar.
+
+    for(int i = 0; i < l; i++) {
+      for(int j = 0; j < c; j++) {
+        mat[i][j] = 0;
+      }
+    }
+
+    for(int i = 0; i < l; i++) {
+      for(int j = 0; j < c; j++) {
+        printf("%d ", mat[i][j]);
+        
+        if(j == c-1) {
+          printf("\n");
+        }
+      }
+    }
+
+    for(int i = 0; i < l; i++) {
+      free(mat[i]);
+    }
+    free(mat);
     return 0;
 }
