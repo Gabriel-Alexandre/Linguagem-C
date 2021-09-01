998e334d84438bd0ebb83fe7b85431365380cfb6
Gabriel-Alexandre <biellalex86@gmail.com>
2021-09-01 15:46:03 -0300

    alteracao Q_12

--- Estrutura_Dados/Lista_01/Q_13/q_13.c
+++ Estrutura_Dados/Lista_01/Q_13/q_13.c
@@ -4,21 +4,30 @@
 int main() {
     char input[30], search;
     int count = 0;
+    char base[] = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWYZX ";
+    int baseCount[strlen(base)];
+
+    for(int i = 0; i < strlen(base); i++) {
+        baseCount[i] = 0;
+    }
 
     printf("Digite um palavra: ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = 0;
 
-    printf("Busque por um caracter: ");
-    scanf("%c", &search);
-
     for(int i = 0; i < 30; i++) {
-        if(input[i] == search) {
-            count++;
+        for(int j = 0; j < strlen(base); j++) {
+            if(input[i] == base[j]) {
+                baseCount[j]++;
+            }
         }
     }
 
-    printf("(%c) ocorreu %d vezes\n", search, count);
+    for(int i = 0; i < strlen(base); i++) {
+        if(baseCount[i] != 0) {
+            printf("(%c) ocorreu %d vezes\n", base[i], baseCount[i]);
+        }
+    }
 
     return 0;
 }
