378a8c8c02ef1656e779d3e6e06e56ede6637bef
Gabriel-Alexandre <biellalex86@gmail.com>
2021-09-06 14:17:37 -0300

    bug

--- Revisao_C/Struct/Normal/normal.c
+++ Revisao_C/Struct/Normal/normal.c
@@ -16,7 +16,7 @@ int main() {
     cadastro_t pessoa;
     // struct cadastro pessoa;
 
-    printf("Nomê: ");
+    printf("Nome: ");
     fgets(pessoa.nome, 20, stdin);
     pessoa.nome[strcspn(pessoa.nome, "\n")] = 0;
     // Remover o \n lido pelo fgets()
