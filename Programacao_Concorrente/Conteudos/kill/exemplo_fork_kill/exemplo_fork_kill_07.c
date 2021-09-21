f3c581f04e0b88d2d29a6a27790efc012a72d404
Gabriel-Alexandre <biellalex86@gmail.com>
2021-09-21 11:52:44 -0300

    feat(Programação concorrente): Add exeplos de kill e wait

---
+++ Programacao_Concorrente/Conteudos/kill/exemplo_fork_kill/exemplo_fork_kill_07.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/signal.h>
+
+int main(void) {
+    // int kill(pid_t pid, int sig)
+    // Há uma certa diferança na forma de matar o processo, isso depende do "sig", 
+    // isso vai ser melhor explicado depois. 
+
+    int fork_return;
+
+    printf("[ORIGINAL] Sou o processo original, PID: %d\n", getpid());
+
+    fork_return = fork();
+
+    if (fork_return == 0) {
+        //clonado
+        printf("[CLONADO] processo clonado, PID: %d\n", getpid());
+        printf("Dormindo 30s..\n");
+        sleep(30);
+    }
+    else {
+        char c;
+        int kill_return;
+        //original
+        printf("[ORIGINAL] Vou finalizar o processo clonado\n");
+        printf("[ORIGINAL] PID do processo clonado: %d\n", fork_return);
+        printf("Aperte <ENTER> para finalizar o processo clonado: ");
+        scanf("%c", &c);
+        kill_return = kill(fork_return, SIGHUP);
+        if (!kill_return)
+            printf("[ORIGINAL] Kill realizado com sucesso! PID %d\n",
+                   fork_return);
+        printf("[ORIGINAL] Vou dormir 10s...\n");
+        sleep(10);
+    }
+
+    return 0;
+}
